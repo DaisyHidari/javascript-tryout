ee77a240fde086951f4c48a72a594cb3ee0831f4
Desiree <chuchu930716@gmail.com>
2024-01-30 13:54:02 -0800

    added some javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+/*pseudocode: 1. Create a new repository on GitHub;
+2. Upload necessary files for the workshop;
+3. Create a sring saying: "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+4. Assign three variables containing arithmetic operator such as +, -,*, /;
+5. Add comments;
+6. Create a dialog box;
+7. Submit the GitHub link to the assignment;
+*/
+
+// alert("This is a pop-up message:p")
+
+let num1 = 2*3;
+let num2 = 100 / 10
+let num3 = 23 + 32
+console.log("num");
