a85254c71040651539009c3cd81404352cc97322
Desiree <chuchu930716@gmail.com>
2024-01-31 11:57:42 -0800

    edited

--- script.js
+++ script.js
@@ -7,18 +7,19 @@
 7. Submit the GitHub link to the assignment;
 */
 
-// Next line is for the popup dialog
-alert("This is a pop-up message:p The combination of the lock is 10 - 40 - 39 BTW.");
+// Next line is for the popup dialog (added it to the index.html for a clearer view)
+// alert("The combination of the lock is 10 - 40 - 39.");
 
 
 // Next section is for the key combinations 10 and 40 and 39.
 let num1 = 2 * 5;
 let num2 = 50 - 10;
 let num3 = 23 + 16;
-console.log("num");
 
+// The next console.log does not really mean anything. I did not use it, but left here for reference.
+// console.log("num");
 
-let dog = 'buddy'
-let iggy = 'logan'
+console.log(typeof num1);
+console.log(10);
 // The following line contains a string for the secret combination;
 console.log('You have received this message because you have been chosen to open an important vault. Here is the secret combination: ' + num1  + "-" + num2 + "-" + num3);
